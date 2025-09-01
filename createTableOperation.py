ea07325b20ac817e8c234faf32ac5c29bfcdc3a7
Mazhar <mazhartuhin8291@gmail.com>
2025-09-02 04:36:39 +0600

    end of Api Practice

--- createTableOperation.py
+++ createTableOperation.py
@@ -9,25 +9,61 @@ def createTables():
 
     cursor.execute('''
 
-    CREATE TABLE IF NOT EXISTS Users(
+            CREATE TABLE IF NOT EXISTS Users(
 
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id VARCHAR(255),
-        password VARCHAR(255),
-        date_of_creation  DATE,
-        isApproved BOOLEAN,
-        blocked BOOLEAN,
-        name VARCHAR(255),
-        address VARCHAR(255),
-        email VARCHAR(255),
-        phone_number VARCHAR(255),
-        pin_code VARCHAR(255)
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id VARCHAR(255),
+                password VARCHAR(255),
+                date_of_creation  DATE,
+                isApproved BOOLEAN,
+                blocked BOOLEAN,
+                name VARCHAR(255),
+                address VARCHAR(255),
+                email VARCHAR(255),
+                phone_number VARCHAR(255),
+                pin_code VARCHAR(255)
 
 
-    )
+            )
 
-    '''
+        ''')
+    
 
-    )
+    cursor.execute('''
+
+        CREATE TABLE IF NOT EXISTS Sell_History(
+                   
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   sell_id VARCHAR(255),
+                   product_id VARCHAR(255),
+                   quantity INT,
+                   remaining_stock INT,
+                   date_of_sell DATE,
+                   price FLOAT,
+                   product_name VARCHAR(255),
+                   user_id VARCHAR(255)
+                   )
+
+''')
+    
+
+    
+    cursor.execute('''
+
+        CREATE TABLE IF NOT EXISTS User_Stock(
+                   
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   product_id VARCHAR(255),
+                   product_name VARCHAR(255),
+                   stock INT,
+                   price FLOAT,
+                   product_name VARCHAR(255),
+                   user_id VARCHAR(255),
+                   user_name VARCHAR(255)
+                   )
+
+''')
+
+    
     conn.commit()
     conn.close()
